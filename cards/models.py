8f7d6b407a5c9039fa5a11b1c61ad6c119c2182e
Adrian Etenyi <etadriano2@gmail.com>
2020-06-10 17:38:49 +0300

    add profile model

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    pic=models.ImageField(upload_to='profile/',blank=True)
+    bio=models.CharField(max_length=30)
+    userId=models.IntegerField()
+
+
+    def __str__(self):
+        return self.bio
+
+    class Meta:
+        ordering=['pic']
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        profile=Profile.objects.all().delete()
+        return profile
+    
