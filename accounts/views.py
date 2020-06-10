5fb0cc8b6a81c7a07ff6a17e90953b2df2be9740
Adrian Etenyi <etadriano2@gmail.com>
2020-06-10 16:49:09 +0300

    add login view that checks if a user is valid

--- accounts/views.py
+++ accounts/views.py
@@ -18,3 +18,18 @@ def signup_view(request):
 
         form = UserCreationForm()
     return render(request,'accounts/signup.html',{'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user() # retrieves the user that is logging in
+            login(request, user) #loges user in if form is valid
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next')) #get the value that accompanied the request and redirects to that value
+            else:                                #next is the name if the input type btw
+              return redirect("home")
+    else:
+
+        form = AuthenticationForm()
+    return render(request,'accounts/login.html',{"form":form})
